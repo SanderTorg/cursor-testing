d3f0b6bd24f68f86204a9578abab5b7139d47df4
SanderTorg <santorgersen@gmail.com>
2025-03-26 14:09:34 +0100

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "/",
   build: {
+    rollupOptions: {
+      input: {
+        index: "./index.html",
+      },
+    },
     outDir: "dist",
     assetsDir: "assets",
     sourcemap: true,
